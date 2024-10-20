8c531afe6a080a66de24337da403f3725d9c2219
ale munden <amunden@my.bcit.ca>
2024-10-20 15:55:53 -0700

    adding contact page

--- react/src/pages/Contact.jsx
+++ react/src/pages/Contact.jsx
@@ -4,7 +4,29 @@ import './styles/Contact.css';
 const Contact = () => {
     return (
         <div className='contact'>
-            <h1>Contact Page</h1>
+            <div className='contact-container'>
+                <div className='title-container'>
+                    <h1 className='title-text'>Have a Question?</h1>
+                    <h2 className='subtitle-text'>Contact us here!</h2>
+                </div>
+                <div className='form-container'>
+                    <form className='contact-form'>
+                        <div className='form-row'>
+                            <label>First Name:</label>
+                            <input className='form-text-input' type='text' required></input>
+                        </div>
+                        <div className='form-row'>
+                            <label>Last Name:</label>
+                            <input className='form-text-input' type='text' required></input>
+                        </div>
+                        <div className='form-row'>
+                            <label>Email:</label>
+                            <input className='form-text-input' type='email' required></input>
+                        </div>
+                        <button type='submit' className='button'>Submit</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
