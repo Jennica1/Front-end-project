6e1e4ed32aa6ea27ba7b5b1aad797259d8e1476c
Brian Paragas <brianparagas49@gmail.com>
2024-10-20 21:39:47 -0700

    added logo and adjusted styling

--- react/src/Components/Header/Header.jsx
+++ react/src/Components/Header/Header.jsx
@@ -5,13 +5,17 @@ import './Header.css';
 const Header = () => {
     return (
         <header className="header">
-            <nav>
-                <ul className="nav-list">
-                    <li className="nav-item"><Link to="/">Home</Link></li>
-                    <li className="nav-item"><Link to="/account">Account</Link></li>
-                    <li className="nav-item"><Link to="/settings">Settings</Link></li>
-                </ul>
-            </nav>
+            <div className="header-container">
+            <Link to="/">
+                <img src="./images/logo.png" alt="Logo" className="logo" /> 
+            </Link>
+                <nav>
+                    <ul className="nav-list">
+                        <li className="nav-item"><Link to="/account">Account</Link></li>
+                        <li className="nav-item"><Link to="/settings">Settings</Link></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 };
