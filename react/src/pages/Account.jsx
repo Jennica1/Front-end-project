00d049ca18bc916018e35310bea5e2e1f68f80ca
kennyharman <144394021+kennyharman@users.noreply.github.com>
2024-12-02 01:22:18 -0800

    adding content to feed and updating profile

--- react/src/pages/Account.jsx
+++ react/src/pages/Account.jsx
@@ -6,7 +6,7 @@ const Account = () => {
         <div className='account'>
             <div className='profile-header'>
                 <div className='profile-image'>
-                    <img src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png" width={150} alt="profile" />
+                    <img src="https://cdn.discordapp.com/attachments/1283117600431276174/1300628114350542888/IMG_6479.jpg?ex=674e5ad2&is=674d0952&hm=a0025a4268c8f9a8e749e11ec07851c6e2ab2657e5ae0c3f6166ad58cf59c801&" width={150} alt="profile" />
                 </div>
                 <div className='profile-info'>
                     <h2 className='profile-username'>DJ_MIXX</h2>
@@ -19,12 +19,12 @@ const Account = () => {
                 </div>
             </div>
             <div className='profile-bio'>
-                <h3>hi guys</h3>
+                <h3>hi music fellas! let's get groovy!</h3>
             </div>
 
             <div className='post-grid'>
-                <div className='post-item'><img src="https://via.placeholder.com/150" alt="post" /></div>
-                <div className='post-item'><img src="https://via.placeholder.com/150" alt="post" /></div>
+                <div className='post-item'><img src="https://i.scdn.co/image/ab6761610000e5ebd77a9c855001f3a9b5815bc0" alt="100gecs_playlist" /></div>
+                <div className='post-item'><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2-O0mtiOFEWaav-mZSxfY9nwsN5FHdD7pVA&s" alt="rap_playlist" /></div>
             </div>
         </div>
     );
